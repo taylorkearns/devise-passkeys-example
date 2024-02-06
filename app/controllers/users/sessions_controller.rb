51277625d0e66f85edf49d5a01825f6c30a4f683
Jack Curtis <jack@gnar.dog>
2024-02-06 08:16:22 -0500

    do only necessary customization of sessions_controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,25 +3,6 @@
 class Users::SessionsController < Devise::SessionsController
   before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  def create
-    self.resource = warden.authenticate!(auth_options)
-
-    sign_in(resource_name, resource)
-
-    render json: resource
-  end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
   protected
 
   def configure_sign_in_params
